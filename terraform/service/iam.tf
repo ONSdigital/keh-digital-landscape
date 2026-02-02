27fc07bd6ac6b906f3f36d8f6f86da1881e47221
Hadi <76945238+hadiqur@users.noreply.github.com>
2026-02-02 13:11:39 +0000

    KEH-1706 - Credential Refactor (AWS) (#210)
    * update: digiland terraform refactor
    
    * update: rename assignation of IAM user to group
    
    * lint: run terraform fmt
    
    * update: fix references to group and specify paths
    
    * update: fix path
    
    * update: rename iam group for consistency

--- terraform/service/iam.tf
+++ terraform/service/iam.tf
@@ -16,10 +16,11 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
-# Attach CloudWatch Logs policy to the task role
-resource "aws_iam_role_policy" "task_logs_policy" {
-  name = "${var.domain}-${var.service_subdomain}-logs-policy"
-  role = aws_iam_role.ecs_task_role.id
+# Managed policy for CloudWatch Logs Access
+resource "aws_iam_policy" "task_logs_policy" {
+  name        = "${var.domain}-${var.service_subdomain}-logs-policy"
+  path        = "/digital-landscape/"
+  description = "Cloudwatch Logs Access"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -37,10 +38,11 @@ resource "aws_iam_role_policy" "task_logs_policy" {
   })
 }
 
-# Custom policy for S3 read and write access to specific bucket
-resource "aws_iam_role_policy" "s3_read_only" {
-  name = "${var.domain}-${var.service_subdomain}-s3-read-write"
-  role = aws_iam_role.ecs_task_role.id
+# Managed policy for S3 read and write access to specific bucket
+resource "aws_iam_policy" "s3_read_and_write" {
+  name        = "${var.domain}-${var.service_subdomain}-s3-read-write"
+  path        = "/digital-landscape/"
+  description = "Read and write to specific bucket"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -65,9 +67,10 @@ resource "aws_iam_role_policy" "s3_read_only" {
 }
 
 # Policy for S3 read access to copilot historic bucket
-resource "aws_iam_role_policy" "s3_copilot_read_only" {
-  name = "${var.domain}-${var.service_subdomain}-s3-copilot-read-only"
-  role = aws_iam_role.ecs_task_role.id
+resource "aws_iam_policy" "s3_copilot_read_only" {
+  name        = "${var.domain}-${var.service_subdomain}-s3-copilot-read-only"
+  path        = "/digital-landscape/"
+  description = "Read-only access to copilot historic bucket"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -89,9 +92,10 @@ resource "aws_iam_role_policy" "s3_copilot_read_only" {
 }
 
 # IAM policy to allow ECS task to access the specified Secrets Manager secret
-resource "aws_iam_role_policy" "secretsmanager_access" {
-  name = "${var.domain}-${var.service_subdomain}-secretsmanager-access"
-  role = aws_iam_role.ecs_task_role.id
+resource "aws_iam_policy" "secretsmanager_access" {
+  name        = "${var.domain}-${var.service_subdomain}-secretsmanager-access"
+  path        = "/digital-landscape/"
+  description = "Access specified Secrets Manager secret"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -106,4 +110,67 @@ resource "aws_iam_role_policy" "secretsmanager_access" {
       }
     ]
   })
+}
+
+# Attach managed policies to ECS Task Role
+resource "aws_iam_role_policy_attachment" "ecs_task_logs_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.task_logs_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_s3_read_and_write_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.s3_read_and_write.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_s3_copilot_read_only_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.s3_copilot_read_only.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secretsmanager_access_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.secretsmanager_access.arn
+}
+
+# IAM User Group
+resource "aws_iam_group" "group" {
+  name = "${var.domain}-${var.service_subdomain}-user-group"
+  path = "/digital-landscape/"
+}
+
+# Attach managed policies to user group
+resource "aws_iam_group_policy_attachment" "group_task_logs_attach" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.task_logs_policy.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_s3_read_and_write_attach" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.s3_read_and_write.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_s3_copilot_read_only_attach" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.s3_copilot_read_only.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_secretsmanager_access_attach" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.secretsmanager_access.arn
+}
+
+# IAM User
+resource "aws_iam_user" "user" {
+  name = "${var.domain}-${var.service_subdomain}"
+  path = "/digital-landscape/"
+}
+
+# Assign IAM User to group
+resource "aws_iam_user_group_membership" "user_group_attach" {
+  user = aws_iam_user.user.name
+
+  groups = [
+    aws_iam_group.group.name
+  ]
 }
