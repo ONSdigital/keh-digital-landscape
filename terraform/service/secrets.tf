aedca48f96cb20db13c8fd4437ce08885cfda96b
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-09 17:43:59 +0000

    update: prevent secrets from being destroyed

--- terraform/service/secrets.tf
+++ terraform/service/secrets.tf
@@ -2,9 +2,17 @@
 resource "aws_secretsmanager_secret" "access_key" {
   name        = "${var.domain}-${var.service_subdomain}-digital-landscape-access-key"
   description = "Access Key ID for digital landscape IAM user"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_secretsmanager_secret" "secret_key" {
   name        = "${var.domain}-${var.service_subdomain}-digital-landscape-secret-key"
   description = "Secret Access Key for digital landscape IAM user"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
