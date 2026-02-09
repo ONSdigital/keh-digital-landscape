31a18cad750bef4bf666aa405fb4d479848ec767
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-09 17:59:39 +0000

    update: further comments

--- terraform/service/secrets.tf
+++ terraform/service/secrets.tf
@@ -1,4 +1,4 @@
-# Secrets for IAM user access keys
+# Secrets for rotated IAM user access keys
 resource "aws_secretsmanager_secret" "access_key" {
   name        = "${var.domain}-${var.service_subdomain}-digital-landscape-access-key"
   description = "Access Key ID for digital landscape IAM user"
