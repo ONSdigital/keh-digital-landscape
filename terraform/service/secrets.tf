7afd89396623c49f6a826cd1201617cfe8ddcb3e
Hadi <76945238+hadiqur@users.noreply.github.com>
2026-02-10 14:00:21 +0000

    KEH-1706: AWS Key Rotation (#213)
    * feat: aws access and secret key rotation
    
    * fix: use root path
    
    * update: remove key vars
    
    * update: prevent secrets from being destroyed
    
    * update: prevent iam user from being destroyed + comment out module
    
    * update: further comments
    
    * update: further comments
    
    * update: further comments
    
    * revert: undo lifecycle changes
    
    ---------
    
    Co-authored-by: Kieran Pritchard <kieran.pritchard@ons.gov.uk>

---
+++ terraform/service/secrets.tf
@@ -0,0 +1,10 @@
+# Secrets for rotated IAM user access keys
+resource "aws_secretsmanager_secret" "access_key" {
+  name        = "${var.domain}-${var.service_subdomain}-digital-landscape-access-key"
+  description = "Access Key ID for digital landscape IAM user"
+}
+
+resource "aws_secretsmanager_secret" "secret_key" {
+  name        = "${var.domain}-${var.service_subdomain}-digital-landscape-secret-key"
+  description = "Secret Access Key for digital landscape IAM user"
+}
