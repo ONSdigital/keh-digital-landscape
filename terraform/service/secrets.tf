6f9e2bfedce8cd96d03467e325475aafe5e5b6a4
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-12 16:49:37 +0000

    fix: remove duplicate name

--- terraform/service/secrets.tf
+++ terraform/service/secrets.tf
@@ -1,10 +1,10 @@
 # Secrets for rotated IAM user access keys
 resource "aws_secretsmanager_secret" "access_key" {
-  name        = "${var.domain}-${var.service_subdomain}-digital-landscape-access-key"
+  name        = "${var.domain}-${var.service_subdomain}-access-key"
   description = "Access Key ID for digital landscape IAM user"
 }
 
 resource "aws_secretsmanager_secret" "secret_key" {
-  name        = "${var.domain}-${var.service_subdomain}-digital-landscape-secret-key"
+  name        = "${var.domain}-${var.service_subdomain}-secret-key"
   description = "Secret Access Key for digital landscape IAM user"
 }
