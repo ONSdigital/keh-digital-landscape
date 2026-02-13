0d3f773ee028d4af44088d88cf5f228d3826c87a
Cooper Wright <cooper.wright@ons.gov.uk>
2026-02-13 11:29:38 +0000

    fix: adding descriptions to outputs for KICS

--- terraform/authentication/outputs.tf
+++ terraform/authentication/outputs.tf
@@ -1,35 +1,44 @@
 output "cognito_reviewer_user_pool" {
-  value = module.cognito.user_pool
+  description = "Cognito reviewer user pool object."
+  value       = module.cognito.user_pool
 }
 
 output "cognito_reviewer_user_pool_id" {
-  value = module.cognito.user_pool_id
+  description = "Cognito reviewer user pool ID."
+  value       = module.cognito.user_pool_id
 }
 
 output "cognito_reviewer_user_pool_arn" {
-  value = module.cognito.user_pool_arn
+  description = "Cognito reviewer user pool ARN."
+  value       = module.cognito.user_pool_arn
 }
 
 output "cognito_reviewer_user_pool_domain" {
-  value = module.cognito.user_pool_domain
+  description = "Cognito reviewer user pool domain."
+  value       = module.cognito.user_pool_domain
 }
 
 output "cognito_reviewer_user_pool_client" {
-  value = module.cognito.user_pool_client
+  description = "Cognito reviewer user pool client object."
+  value       = module.cognito.user_pool_client
 }
 
 output "cognito_reviewer_user_pool_client_id" {
-  value = module.cognito.user_pool_client_id
+  description = "Cognito reviewer user pool client ID."
+  value       = module.cognito.user_pool_client_id
 }
 
 output "cognito_user_groups" {
-  value = module.cognito.user_groups
+  description = "Cognito user group definitions."
+  value       = module.cognito.user_groups
 }
 
 output "cognito_user_pool_domain" {
-  value = module.cognito.user_pool_domain
+  description = "Cognito user pool domain."
+  value       = module.cognito.user_pool_domain
 }
 
 output "cognito_user_pool_sign_out_urls" {
-  value = module.cognito.sign_out_urls
+  description = "Cognito user pool sign-out URLs."
+  value       = module.cognito.sign_out_urls
 }
