00aa9f9795d7021a978fe32dad8c61a0860071c2
Cooper Wright <cooper.wright@ons.gov.uk>
2026-02-19 16:59:07 +0000

    fix: returned to http

--- terraform/service/alb.tf
+++ terraform/service/alb.tf
@@ -4,7 +4,7 @@
 resource "aws_lb_target_group" "frontend_tg" {
   name        = "${var.service_subdomain}-front-farg-tg"
   port        = var.frontend_port
-  protocol    = "HTTPS"
+  protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = data.terraform_remote_state.ecs_infrastructure.outputs.vpc_id
 
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "frontend_tg" {
 resource "aws_lb_target_group" "backend_tg" {
   name        = "${var.service_subdomain}-back-farg-tg"
   port        = var.backend_port
-  protocol    = "HTTPS"
+  protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = data.terraform_remote_state.ecs_infrastructure.outputs.vpc_id
 
