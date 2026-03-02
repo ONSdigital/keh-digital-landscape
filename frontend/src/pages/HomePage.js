68bf7c122ff4a472ec90f3eaa37a09dda8d502a1
@sebtheo <Sebastian.Evison-Theodossiades@ons.gov.uk>
2026-03-02 11:58:29 +0000

    fix: rm duplicate nav-cards

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -119,23 +119,6 @@ function HomePage() {
                 </div>
                 <p>Manage system-wide settings and configurations.</p>
               </a>
-              <a className="nav-card" href="/copilot">
-                <div className="nav-card-header">
-                  <VscCopilot />
-                  <h2>GitHub Copilot</h2>
-                </div>
-                <p>
-                  Analyse GitHub Copilot usage statistics organisation-wide and
-                  by team.
-                </p>
-              </a>
-              <a className="nav-card" href="/addressbook">
-                <div className="nav-card-header">
-                  <TbAddressBook />
-                  <h2>GitHub Address Book</h2>
-                </div>
-                <p>Translate GitHub Usernames to ONS Staff or vice versa.</p>
-              </a>
             </div>
 
             <RecentBanners />
