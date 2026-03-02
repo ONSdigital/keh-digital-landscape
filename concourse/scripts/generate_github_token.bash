380be6a843cfc6985283a7582f0b98def98772d9
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-03-02 16:17:21 +0000

    fix: change from sh to bash
    - Fixes the process for generating a github access token
    - Switched from sh to bash to match GitHub docs
    - Swap App ID for Client ID

--- concourse/scripts/generate_github_token.bash
+++ concourse/scripts/generate_github_token.bash
@@ -1,41 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eo pipefail
+## Note: This MUST be a bash script, not sh, as it uses bash-specific features such as arrays and process substitution.
+
+set -eu
 
 # This script is used to generate a GitHub token for a given GitHub App
 # It retrieves the App ID and Private Key from the environment and
 # then uses them to generate a token which is printed to the console
 
 # Get App ID from environment variable
-if [[ -z "${GITHUB_APP_ID}" ]]; then
-    echo "GITHUB_APP_ID environment variable is not set. Please set it and try again."
+if [ -z "${GITHUB_CLIENT_ID:-}" ]; then
+    echo "GITHUB_CLIENT_ID environment variable is not set. Please set it and try again."
     exit 1
 fi
 
 # Get the Private Key name from environment variable
-if [[ -z "${GITHUB_APP_PRIVATE_KEY_SECRET_NAME}" ]]; then
+if [ -z "${GITHUB_APP_PRIVATE_KEY_SECRET_NAME:-}" ]; then
     echo "GITHUB_APP_PRIVATE_KEY_SECRET_NAME environment variable is not set. Please set it and try again."
     exit 1
 fi
 
 # Get the App Organisation from environment variable
-if [[ -z "${GITHUB_APP_ORG}" ]]; then
+if [ -z "${GITHUB_APP_ORG:-}" ]; then
     echo "GITHUB_APP_ORG environment variable is not set. Please set it and try again."
     exit 1
 fi
 
-app_id="${GITHUB_APP_ID}"
+client_id="${GITHUB_CLIENT_ID}"
 private_key_secret_name="${GITHUB_APP_PRIVATE_KEY_SECRET_NAME}"
 github_org="${GITHUB_APP_ORG}"
 
 # Check the AWS credentials are set
-if [[ -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+if [ -z "${AWS_ACCESS_KEY_ID:-}" ] || [ -z "${AWS_SECRET_ACCESS_KEY:-}" ]; then
     echo "AWS credentials are not set. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables and try again."
     exit 1
 fi
 
 # Get the private key from AWS Secrets Manager
 private_key=$(aws secretsmanager get-secret-value --secret-id "${private_key_secret_name}" --query SecretString --output text)
+pem=$(printf '%b' "${private_key}")
 
 echo "Retrieved private key from AWS Secrets Manager successfully."
 
@@ -80,21 +83,23 @@ printf '%s\n' "JWT: $JWT"
 
 ## 2. Get Installation ID
 
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${JWT}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/${github_org}/installations | jq -r '.id' > installation_id.txt
+installation_id_response=$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${JWT}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/orgs/${github_org}/installation")
+
+installation_id=$(echo "${installation_id_response}" | jq -r '.id')
 
 ## 3. Get Access Token
 
-curl --request POST \
---url "https://api.github.com/app/installations/$(cat installation_id.txt)/access_tokens" \
+token_response=$(curl --request POST \
+--url "https://api.github.com/app/installations/${installation_id}/access_tokens" \
 --header "Accept: application/vnd.github+json" \
 --header "Authorization: Bearer ${JWT}" \
---header "X-GitHub-Api-Version: 2022-11-28" | jq -r '.token' > token.txt
+--header "X-GitHub-Api-Version: 2022-11-28")
 
-token=$(cat token.txt)
+token=$(echo "${token_response}" | jq -r '.token')
 
 ## 4. Output the token so it can be used by the Concourse pipeline
 
