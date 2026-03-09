0f52510c959b012a4447259c33f0adbc3eeff5fd
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2026-03-09 11:14:45 +0000

    ensure user has to ack banner

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,87 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 import '../../styles/Layout.css';
 import { Toaster } from 'react-hot-toast';
 import { BannerContainer } from '../Banner';
 
-const Layout = ({ children, headerProps = {}, bannerProps = {} }) => {
-  const [bannerOffset, setBannerOffset] = useState(0);
-
-  useEffect(() => {
-    if (!bannerProps?.page) {
-      setBannerOffset(0);
-      return;
-    }
-
-    if (typeof window === 'undefined' || typeof document === 'undefined') {
-      return;
-    }
-
-    let resizeObserver;
-    let mutationObserver;
-    let observedElement = null;
-    let rafId;
-
-    const ResizeObserverImpl = window.ResizeObserver;
-    const MutationObserverImpl = window.MutationObserver;
-
-    const attachResizeObserver = element => {
-      if (element === observedElement) return;
-      if (resizeObserver && observedElement) {
-        try {
-          resizeObserver.unobserve(observedElement);
-        } catch {
-          // ignore
-        }
-      }
-
-      observedElement = element;
-      if (!observedElement) return;
-
-      if (typeof ResizeObserverImpl !== 'undefined') {
-        if (!resizeObserver) {
-          resizeObserver = new ResizeObserverImpl(() => update());
-        }
-        resizeObserver.observe(observedElement);
-      }
-    };
-
-    const update = () => {
-      if (rafId) cancelAnimationFrame(rafId);
-      rafId = requestAnimationFrame(() => {
-        const element = document.querySelector('.banner-fixed');
-        attachResizeObserver(element);
-
-        const nextOffset = element
-          ? Math.ceil(element.getBoundingClientRect().height)
-          : 0;
-
-        setBannerOffset(prev => (prev === nextOffset ? prev : nextOffset));
-      });
-    };
-
-    update();
-    window.addEventListener('resize', update);
-
-    if (typeof MutationObserverImpl !== 'undefined') {
-      mutationObserver = new MutationObserverImpl(update);
-      mutationObserver.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
-    }
-
-    return () => {
-      window.removeEventListener('resize', update);
-      if (mutationObserver) mutationObserver.disconnect();
-      if (resizeObserver) resizeObserver.disconnect();
-      if (rafId) cancelAnimationFrame(rafId);
-    };
-  }, [bannerProps?.page]);
-
+const Layout = ({ children, headerProps = {}, bannerProps = {}}) => {
   return (
-    <div className="layout" style={{ '--banner-offset': `${bannerOffset}px` }}>
+    <div className="layout">
       <Toaster
         position="bottom-right"
         reverseOrder={false}
