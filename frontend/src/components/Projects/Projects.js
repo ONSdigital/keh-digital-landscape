86f037205601fcb66f53f5c3ad675df457f04239
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2026-03-19 11:27:56 +0000

    add failure to test CI

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -899,7 +899,7 @@ const Projects = ({
           </div>
         </div>
 
-        <div className="projects-list" tabIndex={0}>
+        <div className="projects-list">
           {!projectsData ? (
             <div className="projects-loading-skeleton">
               {[...Array(6)].map((_, index) => (
