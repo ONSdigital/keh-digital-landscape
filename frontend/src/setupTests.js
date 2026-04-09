1601eb6f0a7e57fdf6f8e778c1f334f1bce43dc0
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-04-09 17:51:50 +0100

    fix: mock local storage before unit tests
    - adds support for node 25+ where localStorage isn't implemented by jsdom

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -21,3 +21,31 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: vi.fn(),
   })),
 });
+
+// Mock LocalStorage for tests
+// This fixes support for Node 25+ where LocalStorage is not implemented in jsdom
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+Object.defineProperty(window, 'localStorage', {
+  value: new LocalStorageMock(),
+});
