e9bbeb9d5fcbe2fb7768491c102ff8fab9503e0c
Sophie Stone <106412444+smstone0@users.noreply.github.com>
2026-04-10 14:49:58 +0100

    Format

--- frontend/src/pages/RadarPage.js
+++ frontend/src/pages/RadarPage.js
@@ -873,10 +873,11 @@ function RadarPage() {
               {directorateName}
             </div>
             <div className="help-modal-body" style={{ marginTop: '16px' }}>
-              Want to suggest a technology change on the radar? View this {' '}
-              <a href="https://github.com/ONSdigital/keh-tech-radar-submissions" 
-              target="_blank"
-              rel="noopener noreferrer"
+              Want to suggest a technology change on the radar? View this{' '}
+              <a
+                href="https://github.com/ONSdigital/keh-tech-radar-submissions"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 repository
               </a>
