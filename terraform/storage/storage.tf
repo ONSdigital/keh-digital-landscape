8410965d0dd95950ba59eca1416fca6d8e87b63c
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2026-04-23 10:28:39 +0100

    rm account detail

--- terraform/storage/storage.tf
+++ terraform/storage/storage.tf
@@ -48,13 +48,6 @@ resource "aws_s3_bucket_public_access_block" "blocked" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_account_public_access_block" "account_prod_block" {
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt_by_default" {
   bucket = aws_s3_bucket.data_bucket.id
 
